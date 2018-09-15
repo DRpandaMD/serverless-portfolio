58772d503b09bd84256f433e3ebe270a2e110fa7
DRpandaMD <michael.zarate.ce@gmail.com>
2018-09-15 17:05:46 -0500

    created upload script for my built artifacts pumped out of aws codebuild

---
+++ upload_built_portfolio_lambda.py
@@ -0,0 +1,24 @@
+import boto3    #aws library
+import io       #is StringIO in python2
+import zipfile  #zip utlity 
+import mimetypes # will allow us to guess types because AWS doesn't :(
+
+# set up globals
+s3 = boto3.resource('s3')
+portfolio_zip = io.BytesIO()
+
+# set up s3 bucketes and download as object
+portfolio_bucket = s3.Bucket('portfolio.zaratedev.org')
+build_bucket = s3.Bucket('portfoliobuild.zaratedev.org')
+build_bucket.download_fileobj('portfolioBuild.zip', portfolio_zip)
+
+# iterate over contents of zip in memory and upload them
+# from my build bucket to my production bucket
+with zipfile.ZipFile(portfolio_zip) as myZip:
+    for item in myZip.namelist():
+        to_upload_obj = myZip.open(item)
+        portfolio_bucket.upload_fileobj(to_upload_obj, item, 
+            ExtraArgs={'ContentType': mimetypes.guess_type(item)[0]})
+        # by default uploaded contents are set to private we need to change the 'Access Control List'
+        # to enable 'public-read' to make the new uploaded content accessable to the public :)
+        portfolio_bucket.Object(item).Acl().put(ACL='public-read') 
