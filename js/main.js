cf248cf42057f0ca03a9179b502942809e5015d9
DRpandaMD <michael.zarate.ce@gmail.com>
2018-09-21 16:43:34 -0500

    changed boiler plate HTML into React Components

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from "./example-work";
 
-console.log("We loaded react-dom");
+const myWork = [
+    {
+        'title': "",
+        'image': {
+            'desc': "example screenshot of a project involving code",
+            'src': "images/example1.png",
+            'comment': "This is just a screen shot of our code :) "
+        }
+    },
+    {
+        'title': "",
+        'image': {
+            'desc': "example screenshot of a project involving chemistry",
+            'src': "images/example2.png",
+            'comment': "Chemistry” by Surian Soosay is licensed under CC BY 2.0 https://www.flickr.com/photos/ssoosay/4097410999"
+        }
+    },
+    {
+        'title': "",
+        'image': {
+            'desc': "example screenshot of a project involving cats",
+            'src': "images/example3.png",
+            'comment': `"Bengal cat” by roberto shabs is licensed under CC BY 2.0" 
+                        https://www.flickr.com/photos/37287295@N00/2540855181`
+        }
+    }
+]
 
-console.log("Webpack works :D!")
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
