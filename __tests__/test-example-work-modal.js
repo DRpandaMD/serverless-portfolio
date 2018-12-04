521d3f4da4f1b69169ed268029235277b3a2d1f9
DRpandaMD <michael.zarate.ce@gmail.com>
2018-12-04 10:16:56 -0600

    slight refactor to create React.js component and created test cases to test component

--- __tests__/test-example-work-modal.js
+++ __tests__/test-example-work-modal.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ExampleWork from "./example-work";
+import {shallow}  from 'enzyme';
+import ExampleWorkModal from '../js/example-work-modal'
+
+import Enzyme from "enzyme";
+import Adaptor from 'enzyme-adapter-react-16';
+Enzyme.configure({adapter: new Adaptor() });
+
 
 const myWork = [
     {
@@ -22,18 +27,21 @@ const myWork = [
             'src': "images/example2.png",
             'comment': "Chemistry” by Surian Soosay is licensed under CC BY 2.0 https://www.flickr.com/photos/ssoosay/4097410999"
         }
-    },
-    {
-        'title': "Work Example 3",
-        'href' : "https://example.com",
-        'desc' : "Donec ut quam molestie, malesuada libero eu, posuere turpis. Phasellus hendrerit diam in augue molestie condimentum. In eu porta lacus. Curabitur gravida quam ac orci posuere, at tempor sem viverra. Nam et nisi ut ligula consequat mattis vitae quis felis. Ut scelerisque sem enim, volutpat lobortis ante efficitur at. Fusce id enim in tellus porttitor cursus vel non quam. Nam at purus laoreet ante aliquet rutrum. Pellentesque ut nibh sem. Ut id hendrerit felis. Aenean convallis leo eu diam elementum, nec cursus metus vestibulum.",
-        'image': {
-            'desc': "example screenshot of a project involving cats",
-            'src': "images/example3.png",
-            'comment': `"Bengal cat” by roberto shabs is licensed under CC BY 2.0" 
-                        https://www.flickr.com/photos/37287295@N00/2540855181`
-        }
     }
-]
+];
 
-ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
+describe("ExampleWorkModal component", () => {
+
+    let component = shallow(<ExampleWorkModal example = {myWork[0]}/>);
+    let anchors = component.find("a");
+   
+
+    it("Should have one 'a' element", () => {
+        expect(anchors.length).toEqual(1);
+    });
+
+    it("Should link to our project", () => {
+        expect(anchors.prop.href).toEqual(myWork.href);
+    });
+
+});
